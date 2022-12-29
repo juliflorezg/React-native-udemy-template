e89ffe7c0cd3608adb4db46a06e3b7f65a391893
juliflorezg <forezjuli08@hotmail.com>
2022-12-29 15:25:46 -0500

    add icon

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,14 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {View, Text, Button} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 interface Props extends StackScreenProps<any, any> {}
 
 export const HomeScreen = ({navigation}: Props) => {
   return (
     <View>
+      <Icon name="dot-circle" size={30} color="red" />
       <Text>home</Text>
       <Button
         title="Go to Pokemon page"
