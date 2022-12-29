4a4f12e920e954b2df658fb12aff0617d8d44b6a
juliflorezg <forezjuli08@hotmail.com>
2022-12-29 14:34:37 -0500

    Add navigator base files

---
+++ src/screens/PokemonScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+export const PokemonScreen = () => {
+  return (
+    <View>
+      <Text>pokemon info</Text>
+    </View>
+  );
+};
