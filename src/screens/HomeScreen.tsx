62425c6177a8e3992335bc52f7a517b9173db3ec
juliflorezg <forezjuli08@hotmail.com>
2022-12-29 14:48:20 -0500

    add navigation to homeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,17 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {View, Text} from 'react-native';
-export const HomeScreen = () => {
+import {View, Text, Button} from 'react-native';
+
+interface Props extends StackScreenProps<any, any> {}
+
+export const HomeScreen = ({navigation}: Props) => {
   return (
     <View>
       <Text>home</Text>
+      <Button
+        title="Go to Pokemon page"
+        onPress={() => navigation.navigate('PokemonScreen')}
+      />
     </View>
   );
 };
